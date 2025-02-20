a1b84a871e39be3934fdbb93e245193c475f06c6
João Pedro <jpedro1580@gmail.com>
2025-02-19 21:48:38 -0300

    feat(pubsub): adds declaring and binding queues to exchanges

--- internal/pubsub/pubsub.go
+++ internal/pubsub/pubsub.go
@@ -7,6 +7,11 @@ import (
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
+const (
+	QueueTypeTransient = iota
+	QueueTypeDurable
+)
+
 func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
 	body, err := json.Marshal(val)
 	if err != nil {
@@ -17,4 +22,30 @@ func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
 		Body:        body,
 	})
 	return nil
+}
+
+func DeclareAndBind(conn *amqp.Connection,
+	exchange, queueName, key string, simpleQueueType int) (*amqp.Channel, amqp.Queue, error) {
+	channel, err := conn.Channel()
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+	var queue amqp.Queue
+	if simpleQueueType == QueueTypeDurable {
+		queue, err = channel.QueueDeclare(queueName, true, false, false, false, nil)
+		if err != nil {
+			return nil, amqp.Queue{}, err
+		}
+	} else {
+		queue, err = channel.QueueDeclare(queueName, false, true, true, false, nil)
+		if err != nil {
+			return nil, amqp.Queue{}, err
+		}
+	}
+	err = channel.QueueBind(queueName, key, exchange, false, nil)
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+	return channel, queue, nil
+
 }
